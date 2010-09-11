b34a0ce8a5816e0fa81ffc751819c51bb7f0143b
Diego Carrion <dc.rec1@gmail.com>
2010-09-11 18:27:06 -0300

    refactored sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,11 @@
 class SessionsController < Devise::SessionsController
   def require_no_authentication
-    session[:return_url] ||= params[:returnURL]
+    session[:return_url] ||= request_return_url
     super
   end
 
   def after_sign_in_path_for(resource_or_scope)
-    "#{return_url}?token=#{token}"
+    "#{session_return_url}?token=#{token}"
   end
 
   def after_sign_out_path_for(resource_or_scope)
@@ -18,7 +18,11 @@ class SessionsController < Devise::SessionsController
     current_user.new_token_value
   end
 
-  def return_url
+  def request_return_url
+    params[:returnURL]
+  end
+
+  def session_return_url
     session[:return_url]
   end
 end
