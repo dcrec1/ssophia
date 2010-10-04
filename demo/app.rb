dda60e1eab3465c86af06ac86f09c42b1c03d7b5
Diego Carrion <dc.rec1@gmail.com>
2010-10-04 01:59:03 -0300

    improved demo session management

--- demo/app.rb
+++ demo/app.rb
@@ -3,26 +3,47 @@ require 'sinatra'
 require 'haml'
 require 'rest_client'
 require 'crack'
+require 'ruby-debug'
+
+enable :sessions
 
 SSOPHIA_BASE_URL = "http://localhost:3000"
+
+def create_user_session
+  begin
+    json = RestClient.get "#{SSOPHIA_BASE_URL}/tokens/#{params[:token]}.json"
+    session[:user] = Crack::JSON.parse(json)['token']['user']
+    session[:ssophia] = request.cookies['ssophia']
+  rescue
+  end
+end
 
 def current_user
   session[:user]
 end
 
+def cookie_exists?
+  not request.cookies['ssophia'].nil?
+end
+
+def cookie_did_change?
+  request.cookies['ssophia'] != session[:ssophia]
+end
+
+def logged?
+  current_user and cookie_exists? and not cookie_did_change?
+end
+
 get '/' do
-  if params[:token]
-    json = RestClient.get "#{SSOPHIA_BASE_URL}/tokens/#{params[:token]}.json"
-    session[:user] = Crack::JSON.parse(json)['token']['user']
-  end
-  @logged = current_user and not request.cookies['ssophia'].nil?
+  create_user_session if params[:token]
+  @logged = logged?
   haml :index
 end
 
 get '/login' do
-  redirect "#{SSOPHIA_BASE_URL}/users/sign_in?returnURL=http://localhost:3001"
+  redirect "#{SSOPHIA_BASE_URL}/users/sign_in?returnURL=http://localhost:3001/"
 end
 
 get '/logout' do
-  redirect "#{SSOPHIA_BASE_URL}/users/sign_out?returnURL=http://localhost:3001"
+  redirect "#{SSOPHIA_BASE_URL}/users/sign_out?returnURL=http://localhost:3001/"
 end
